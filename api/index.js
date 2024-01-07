99c762790dee5acf69b9a4ff9d95ade8978f54ad
SlaviHristev <Slavihristev97@gmail.com>
2024-01-07 19:47:56 +0200

    Implemented login from client using api

--- api/index.js
+++ api/index.js
@@ -10,8 +10,14 @@ import cookieParser from "cookie-parser";
 
 
 const app = express();
+app.use((req,res,next) =>{
+    res.header("Access-Control-Allow-Credentials",true)
+    next()
+})
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:'http://localhost:5173'
+}));
 app.use(cookieParser())
 db.connect((err) =>{
     if(err){
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,13 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from './Login.module.css'
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 
 export default function Login(){
     const {login} = useContext(AuthContext);
 
-    const handleLogin = () =>{
-        login();
+    const [inputs, setInputs] = useState({
+        username:"",
+        password:"",
+    });
+    const [err, setErr] = useState(null);
+
+    const navigate = useNavigate()
+
+    const handleChange = (e) =>{
+        setInputs(prev=>({...prev, [e.target.name]:e.target.value}));
+        
+    }
+    const handleLogin = async(e) =>{
+        e.preventDefault()
+        try {
+            await login(inputs)
+            navigate('/')
+        } catch (error) {
+            setErr(error.response.data)
+        }
     }
     return(
         
@@ -24,8 +42,9 @@ export default function Login(){
                 <div className={styles.right}>
                     <h1>Login</h1>
                     <form>
-                        <input type="text" placeholder="Username" />
-                        <input type="password" placeholder="Password"/>
+                        <input type="text" placeholder="Username" name='username' onChange={handleChange}/>
+                        <input type="password" placeholder="Password" name='password' onChange={handleChange}/>
+                        {err && err}
                         <button onClick={handleLogin}>Login</button>
                     </form>
                 </div>
