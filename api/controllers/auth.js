545a3263e80cb62e4aafbeec8027cedd894105b1
SlaviHristev <Slavihristev97@gmail.com>
2024-01-10 19:58:07 +0200

    Made share component with styles

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -26,13 +26,14 @@ export const register = (req, res) => {
 
     const q = "SELECT * FROM users WHERE username = ?";
 
-    db.query(q, [req.body.username], async (err, data) => {
+    db.query(q, [req.body.username], (err, data) => {
         if (err) return res.status(500).json(err);
         if (data.length) return res.status(409).json("User already exists!")
 
 
-        const salt = await bcrypt.genSaltSync(10);
-        const hashedPass = await  bcrypt.hashSync(req.body.password, salt)
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPass = bcrypt.hashSync(req.body.password, salt);
+        
 
         const q = "INSERT INTO users (`username`,`email`,`password`,`name`) VALUES (?)";
 
