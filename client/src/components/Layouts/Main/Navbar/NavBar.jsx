0693725d8cfe19a8b115b19cd0fcc2e22478fddf
SlaviHristev <Slavihristev97@gmail.com>
2024-02-08 19:39:22 +0200

    Made some improvments for mobile view

--- client/src/components/Layouts/Main/Navbar/NavBar.jsx
+++ client/src/components/Layouts/Main/Navbar/NavBar.jsx
@@ -31,13 +31,14 @@ export default function NavBar() {
            
                 <div className={styles.navbar}>
                     <div className={styles.left}>
-                        <Link to={'/'} style={{ textDecoration: 'none' }}>
+                        <Link to={'/'} style={{ textDecoration: 'none' }} >
                             <span>Socical Media App</span>
                         </Link>
                         <Link to={'/'} style={{textDecoration:'none', color:'none', fill:'none'}}><HomeIcon style={{height:'50px', paddingTop:'5px'}}  /></Link>
                         {darkMode ? <DarkModeIcon onClick={toggle} />
                             : <LightModeIcon onClick={toggle} />}
-                        <GridViewIcon />
+                            <Link style={{textDecoration:'none'}} to={'/messenger'}><EmailIcon /></Link>
+                        
                         <div className={styles.search}>
                             <SearchIcon />
                             <input type="text" placeholder='Search' />
@@ -46,7 +47,7 @@ export default function NavBar() {
                     <div className={styles.right}>
                         <PersonIcon style={{cursor:"pointer"}} onClick={() => setDropDown(!dropDown)}/>
                         {dropDown && <DropDownMenu userId={currentUser.id}/>}
-                        <Link to={'/messenger'}><EmailIcon /></Link>
+                        <GridViewIcon />
                         <NotificationsIcon />
                         <Link style={{textDecoration:'none'}} to={'/profile/'+ currentUser.id}><div className={styles.user}>
                             <img src={"/upload/"+currentUser.profilePic} alt="" />
